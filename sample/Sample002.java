4c7543daca543cc946482305f2ca8e18342b2d45
井上雄太 <s1341011we@gmail.com>
2023-07-30 02:47:08 +0900

    [day1]

---
+++ sample/Sample002.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Sample002 {
+	
+	public static void main(String[] args) {
+		
+		System.out.print(123);
+		System.out.println(456);
+		System.out.print("ABC");
+		System.out.println("DEF");
+	}
+
+}
--- sample/sample001.java
+++ sample/sample001.java
@@ -1,5 +1,10 @@
 package sample;
 
 public class sample001 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello,World.");
+	}
 
 }
