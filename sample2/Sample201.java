9bafd46133b9607511c189818fd0f5de786b8f89
井上雄太 <s1341011we@gmail.com>
2023-08-05 03:08:43 +0900

    [SampleDay2]

---
+++ sample2/Sample201.java
@@ -0,0 +1,19 @@
+package sample2;
+
+public class Sample201 {
+	
+	public static void main (String[] args) {
+		
+		System.out.print(5 + " + " + 2 + " = ");
+		System.out.println(5+2);
+		System.out.print(5 + " - " + 2 + " = ");
+		System.out.println(5-2);
+		System.out.print(5 + " * " + 2 + " = ");
+		System.out.println(5*2);
+		System.out.print(5 + " / " + 2 + " = ");
+		System.out.print(5/2);
+		System.out.println("余り" + 5%2);
+		
+	}
+
+}
