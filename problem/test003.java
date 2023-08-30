b00f75a0a45d135279d7aedae1ebb96908c0481f
井上雄太 <s1341011we@gmail.com>
2023-08-31 04:33:21 +0900

    [test003]

--- problem/test003.java
+++ problem/test003.java
@@ -1,5 +1,16 @@
 package problem;
 
 public class test003 {
+	
+	public static void main(String [] args) {
+		
+		for(int n = 1; n <= 10; n++) {
+			System.out.print(n);
+			if(n < 10) {
+				System.out.print(",");
+			}
+		}
+		
+	}
 
 }
