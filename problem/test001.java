9b260707aa1acc8479a7747c819f104661c2376d
井上雄太 <s1341011we@gmail.com>
2023-08-31 03:42:44 +0900

    [test001]

---
+++ problem/test001.java
@@ -0,0 +1,13 @@
+package problem;
+
+public class test001 {
+
+	public static void main(String[] args) {
+
+		for (int n = 1; n <= 10; n++) {
+			if (n % 2 == 0) {
+				System.out.print(n + " ");
+			}
+		}
+	}
+}
